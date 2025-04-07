45872d5cfa17927230d3476353c908a86ec16b8e
MedAly666 <medaly700222@gmail.com>
2025-04-07 13:35:03 +0100

    remove unused imports from test classes

--- demo/src/test/java/com/akeb/TP0/exo3/FibonacciTest.java
+++ demo/src/test/java/com/akeb/TP0/exo3/FibonacciTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import com.akeb.TP0.exo3.Fibonacci;
 
 public class FibonacciTest {
 
